75eb9a4793c18570c2b68d5f78b5c63e62de8efa
itamargs <itamargs@gmail.com>
2019-12-25 01:33:09 +0200

    ADD: main webrtc logic + user interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+// Main logic for the angular video chat app
+
+//imports declerations for Easyrtc
+/// <reference path="../../../web-rtc/node_modules/easyrtc/typescript_support/d.ts.files/client/easyrtc.d.ts" />>
+import {Component, ChangeDetectorRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
+
 export class AppComponent {
-  title = 'webrtc-videoChat';
+
+  constructor(private cdr: ChangeDetectorRef) {
+  }
+
+  myId: string = '';
+  connectedClientsList: Array<string> = [];
+
+  // clear connected peers in the starts of the app or refresh
+  clearConnectList(): void {
+    this.connectedClientsList = [];
+    this.cdr.detectChanges();
+  }
+
+  // calling the selected peer after pressing on the call button
+  performCall(clientEasyrtcId: string): void {
+    let successCB = function(a: string, b: string): void {
+    };
+    let failureCB = function(a: string, b: string): void {
+    };
+    easyrtc.call(clientEasyrtcId, successCB, failureCB, undefined, undefined);
+  }
+
+  buildCaller(easyrtcid: string): (() => void) {
+    return (): void => {
+      this.performCall(easyrtcid);
+    };
+  }
+
+  convertListToButtons(roomName: string, data: Easyrtc_PerRoomData, isPrimary: boolean): void {
+    this.clearConnectList();
+    for (let easyrtcid in data) {
+      this.connectedClientsList.push(easyrtc.idToName(easyrtcid));
+    }
+    this.cdr.detectChanges();
+  }
+
+  // updating current user ID (will give new id on refresh)
+  updateMyEasyRTCId(myEasyRTCId: string): void {
+    this.myId = myEasyRTCId;
+    this.cdr.detectChanges();
+  }
+
+  loginSuccess(easyrtcid: string): void {
+    this.updateMyEasyRTCId(easyrtc.cleanId(easyrtcid));
+  }
+
+  loginFailure(errorCode: string, message: string): void {
+    this.updateMyEasyRTCId('Login failed. Reason: ' + message);
+  }
+
+  // actual logic for connecting between peers
+  connect(): void {
+    easyrtc.setVideoDims(320, 240, undefined);
+    let convertListToButtonShim = (roomName: string, data: Easyrtc_PerRoomData, isPrimary: boolean): void => {
+      this.convertListToButtons(roomName, data, isPrimary);
+    };
+
+    // connecting to the node.js server via secure connection (here on heroku)
+    easyrtc.setSocketUrl('https://vast-bastion-49340.herokuapp.com', {secure: true});
+    // easyrtc.setSocketUrl('https://localhost:5000', {rejectUnauthorized: false});
+    easyrtc.setRoomOccupantListener(convertListToButtonShim);
+    easyrtc.easyApp('easyrtc.audioVideoSimple', 'videoSelf', ['videoCaller'], this.loginSuccess.bind(this), this.loginFailure.bind(this));
+  }
+
+  ngAfterViewInit() {
+    this.connect();
+  }
+
 }
